170cff3c3ceb7d364a63f869ee4be15f83ea994f
Roman Nosov <romannosov_2001@mail.ru>
2023-08-16 22:12:15 +0300

    Initial commit

---
+++ src/main/java/ru/netollogy/springStartHomeWork/SpringStartHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package ru.netollogy.springStartHomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartHomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartHomeWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netollogy/springStartHomeWork/SpringStartHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netollogy.springStartHomeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartHomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
