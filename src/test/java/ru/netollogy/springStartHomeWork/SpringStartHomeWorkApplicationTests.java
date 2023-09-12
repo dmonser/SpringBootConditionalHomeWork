0c72e3d0f945714edba5b708f3991afa8f2eafe1
Роман Носов <romannosov_2001@mail.ru>
2023-09-12 17:49:09 +0300

    Added integration test

--- src/test/java/ru/netollogy/springStartHomeWork/SpringStartHomeWorkApplicationTests.java
+++ src/test/java/ru/netollogy/springStartHomeWork/SpringStartHomeWorkApplicationTests.java
@@ -1,13 +1,46 @@
 package ru.netollogy.springStartHomeWork;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringStartHomeWorkApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+	private static final GenericContainer<?> devApp = new GenericContainer<>("devapp:latest")
+			.withExposedPorts(8080);
+	private static final GenericContainer<?> prodApp = new GenericContainer<>("prodapp:latest")
+			.withExposedPorts(8081);
+	@BeforeAll
+	public static void setUp() {
+		devApp.start();
+		prodApp.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void contextLoadsDevApp() {
+		ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" +
+				devApp.getMappedPort(8080) + "/profile", String.class);
+		System.out.println(forEntity.getBody());
+
+		Assertions.assertEquals("Current profile is dev", forEntity.getBody());
+	}
+
+	@Test
+	void contextLoadsProdApp() {
+		ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" +
+				prodApp.getMappedPort(8081) + "/profile", String.class);
+		System.out.println(forEntity.getBody());
+
+		Assertions.assertEquals("Current profile is production", forEntity.getBody());
 	}
 
 }
